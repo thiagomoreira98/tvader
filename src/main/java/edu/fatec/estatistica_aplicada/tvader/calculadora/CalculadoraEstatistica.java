5e31ec99137000e364ca26002169426c7467c33a
Tiago On SSD Ubuntu <tiago@ti2.net.br>
2017-02-11 12:59:03 -0200

    Arquvivo de suporte a compartilhamento de calculos já prontos

---
+++ src/main/java/edu/fatec/estatistica_aplicada/tvader/calculadora/CalculadoraEstatistica.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+package edu.fatec.estatistica_aplicada.tvader.calculadora;
+
+
+
+import edu.fatec.estatistica_aplicada.tvader.calculo.Amostragem;
+import edu.fatec.estatistica_aplicada.tvader.calculo.NormalizarErro;
+
+/**
+ *
+ * @author tiago
+ */
+public class CalculadoraEstatistica {
+ 
+    public Amostragem amostragem = new Amostragem();
+    public NormalizarErro erro_n0 = new NormalizarErro();
+}
